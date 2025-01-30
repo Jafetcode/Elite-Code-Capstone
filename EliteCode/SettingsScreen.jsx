b5805ccea67ff516d3a275954180698bd7cf8d38
evanwells34 <ewells0709@gmail.com>
2025-01-30 15:13:40 -0600

    Updated Setting Screen
    Added list of potential settings to feature

--- EliteCode/SettingsScreen.jsx
+++ EliteCode/SettingsScreen.jsx
@@ -2,24 +2,46 @@ import * as React from 'react';
 import { Text } from 'react-native';
 import {useNavigation,} from '@react-navigation/native';
 import {default as theme} from './custom-theme.json'
-import { ApplicationProvider,Button,Layout } from '@ui-kitten/components';
+import { ApplicationProvider,Button,Layout, ListItem } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
 
-function SettingsScreen() {
-  const navigation = useNavigation();
+  
+function SettingsScreen(){
+    const navigation = useNavigation();
+    return (
+        <Layout>
+      <ListItem title="Profile" description="Manage your account" />
+     
+      <ListItem title="Notifications" description="Turn on or off" />
+    
+      <ListItem title="Font-size" description="Change font size" />
 
-  return (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>SettingsScreen</Text>
-      <Button onPress={() => navigation.push('Home')}>
-        Go to Home
-      </Button>
-      <Button onPress={() => navigation.goBack()}>Go back</Button>
-    </Layout>
-  );
+      <ListItem title="Security" description="Change your password" />
+    
+      <ListItem title="Brightness" description="Light or dark mode" />
+      
+      <ListItem title="Language" description="Change language" />
+  
+      <ListItem title="Help" description="Get support" />
+   
+      <ListItem title="About" description="App information" />
+
+                    <Button onPress={() => navigation.navigate('Home')}>
+                    Go to Home
+                    </Button>
+                    </Layout>
+        );
 }
-  export default ()=> (
-    <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+export default ()=> (
+    <ApplicationProvider {...eva} theme={{...eva.dark, ...theme}}>
+        <Layout style={
+            { flex: 1, 
+                // justifyContent: 'center', 
+                // alignItems: 'center' 
+            }
+            }>
         <SettingsScreen/>
+        </Layout>
     </ApplicationProvider>
+
 );
