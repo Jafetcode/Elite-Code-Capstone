dedddb892a84465d0f38f581ab9f87f5af52279c
evanwells34 <ewells0709@gmail.com>
2025-02-14 10:16:52 -0600

    Create Question Page
    created create question page

--- EliteCode/TeacherView/TeacherCreateQuestion.jsx
+++ EliteCode/TeacherView/TeacherCreateQuestion.jsx
@@ -1,31 +1,60 @@
 import * as React from "react";
 import { View, ScrollView, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Button, Layout, Text, Card } from "@ui-kitten/components";
+import { Button, Layout, Text, Card, Radio, RadioGroup } from "@ui-kitten/components";
+import { Input } from '@ui-kitten/components';
 
 function TeacherCreateQuestion() {
     const navigation = useNavigation();
+    const [value, setValue] = React.useState('');
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
 
     return (
+        
         <Layout style={{ flex: 1, padding: 20, backgroundColor: "#2C496B" }}>
-
-            <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
-                <Button appearance="ghost" status="basic" onPress={() => navigation.goBack()}>
-                    {"<"}
-                </Button>
-                <Text category="h5" style={{ flex: 1, textAlign: "center", paddingRight: 50 }}>
-                    Elite Code
-                </Text>
-            </View>
-
-            <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
-                <Text category="h5" style={{ flex: 1, textAlign: "center"}}>
-                    Create Question Page
-                </Text>
-            </View>
-
-        </Layout>
-    );
+        
+                    <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
+                        <Button appearance="ghost" status="basic" onPress={() => navigation.goBack()}>
+                            {"<"}
+                        </Button>
+                        <Text category="h3" style={{ flex: 1, textAlign: "center", paddingRight: 50 }}>
+                            Create Question
+                        </Text>
+                    </View>
+        
+                    <ScrollView>
+                        <View style={{ marginBottom: 20 }}>
+        
+                            <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 5 }}>
+                                <Text category="h5">Question *</Text>
+    
+                            </View>
+        
+                            <Input placeholder='Type Question Here' value={value} onChangeText={nextValue => setValue(nextValue)} />
+        
+                            <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 5 }}>
+                                <Text category="h5">Description</Text>
+    
+                            </View>
+        
+                            <Input placeholder='Type Description Here' value={value} onChangeText={nextValue => setValue(nextValue)} />
+                            
+                            <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 5 }}>
+                                <Text category="h5">Type *</Text>
+                            </View>
+                            <RadioGroup selectedIndex={selectedIndex} onChange={index => setSelectedIndex(index)}>
+                                <Radio>
+                                    Multiple Choice
+                                </Radio>
+                                <Radio>
+                                    Short Answer
+                                </Radio>
+                                </RadioGroup>
+                        </View>
+                        <Button> Submit Question</Button>
+                    </ScrollView>
+                </Layout>
+            );
 }
 
 export default () => (
