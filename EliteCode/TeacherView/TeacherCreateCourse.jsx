e13dcc7bf4266eb4fe8d2b3db2548670c3a56398
evanwells34 <ewells0709@gmail.com>
2025-02-19 10:20:01 -0600

    Created "Create Lesson" page
    Did not add "Due Date" yet

--- EliteCode/TeacherView/TeacherCreateCourse.jsx
+++ EliteCode/TeacherView/TeacherCreateCourse.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { View, ScrollView, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { ApplicationProvider, IconRegistry, Layout, Button, Text, Icon, Card } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry, Layout, Button, Text, Icon, Card, Input, Radio, RadioGroup } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
@@ -9,8 +9,11 @@ const BackIcon = (props) => <Icon {...props} name="arrow-back" />;
 
 function TeacherCreateCourse() {
     const navigation = useNavigation();
+    const [value, setValue] = React.useState('');
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
 
     return (
+
         <Layout style={{ flex: 1, padding: 20, backgroundColor: "#2C496B" }}>
 
             <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
@@ -20,17 +23,21 @@ function TeacherCreateCourse() {
                     accessoryLeft={BackIcon}
                     onPress={() => navigation.goBack()}
                 />
-                <Text category="h5" style={{ flex: 1, textAlign: "center", paddingRight: 50 }}>
-                    Elite Code
+                <Text category="h3" style={{ flex: 1, textAlign: "center", paddingRight: 50 }}>
+                    Create Course
                 </Text>
             </View>
 
-            <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
-                <Text category="h5" style={{ flex: 1, textAlign: "center" }}>
-                    Create Course Page
-                </Text>
-            </View>
+            <ScrollView>
+                <View style={{ marginBottom: 20 }}>
 
+                    
+
+                    
+            
+                </View>
+                <Button> Create Course</Button>
+            </ScrollView>
         </Layout>
     );
 }
@@ -43,3 +50,4 @@ export default () => (
         </ApplicationProvider>
     </>
 );
+
--- EliteCode/TeacherView/TeacherCreateLesson.jsx
+++ EliteCode/TeacherView/TeacherCreateLesson.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { View, ScrollView, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { ApplicationProvider, IconRegistry, Layout, Button, Text, Icon, Card } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry, Layout, Button, Text, Icon, Card, Input, Radio, RadioGroup,  } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
@@ -9,8 +9,12 @@ const BackIcon = (props) => <Icon {...props} name="arrow-back" />;
 
 function TeacherCreateLesson() {
     const navigation = useNavigation();
+    const [value, setValue] = React.useState('');
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+
 
     return (
+
         <Layout style={{ flex: 1, padding: 20, backgroundColor: "#2C496B" }}>
 
             <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
@@ -20,17 +24,48 @@ function TeacherCreateLesson() {
                     accessoryLeft={BackIcon}
                     onPress={() => navigation.goBack()}
                 />
-                <Text category="h5" style={{ flex: 1, textAlign: "center", paddingRight: 50 }}>
-                    Elite Code
+                <Text category="h3" style={{ flex: 1, textAlign: "center", paddingRight: 50 }}>
+                    Create Lesson
                 </Text>
             </View>
 
-            <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
-                <Text category="h5" style={{ flex: 1, textAlign: "center" }}>
-                    Create Lesson Page
-                </Text>
-            </View>
+            <ScrollView>
+                <View style={{ marginBottom: 20 }}>
 
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 5 }}>
+                        <Text category="h5">Lesson Name *</Text>
+
+                    </View>
+
+                    <Input placeholder='Type Question Here' value={value} onChangeText={nextValue => setValue(nextValue)} />
+
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 5 }}>
+                        <Text category="h5">Description</Text>
+
+                    </View>
+
+                    <Input placeholder='Type Description Here' value={value} onChangeText={nextValue => setValue(nextValue)} />
+
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 5 }}>
+                        <Text category="h5">Student Access *</Text>
+                    </View>
+                    <RadioGroup selectedIndex={selectedIndex} onChange={index => setSelectedIndex(index)}>
+                        <Radio>
+                            All Access
+                        </Radio>
+                        <Radio>
+                            Custom Access
+                        </Radio>
+                    </RadioGroup>
+                
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 5 }}>
+                        <Text category="h5">Premise *</Text>
+
+                    </View>
+                    <Input placeholder='Write the premise of the questions here' value={value} onChangeText={nextValue => setValue(nextValue)} />
+                </View>
+                <Button> Submit Question</Button>
+            </ScrollView>
         </Layout>
     );
 }
