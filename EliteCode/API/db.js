1a7741ff6c6ba562216c4aa9b7e28aeab88ccad4
evillalpando5 <146878902+evillalpando5@users.noreply.github.com>
2025-03-25 16:51:50 -0500

    Update db.js

--- EliteCode/API/db.js
+++ EliteCode/API/db.js
@@ -12,11 +12,11 @@ const db = mysql.createConnection({
 });
 
 db.connect((err) => {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-  console.log('Connected to the database');
-});
+    if (err) {
+      console.error('Error connecting to the database:', err.stack);
+      return;
+    }
+    console.log('Connected to the database as ID ' + db.threadId);
+  });
 
 module.exports = db;
