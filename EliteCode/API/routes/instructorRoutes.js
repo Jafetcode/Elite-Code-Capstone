cff2fe3db63ff83581782d493cb97dd4eff28d6d
evillalpando5 <146878902+evillalpando5@users.noreply.github.com>
2025-03-25 15:47:59 -0500

    Update instructorRoutes.js

--- EliteCode/API/routes/instructorRoutes.js
+++ EliteCode/API/routes/instructorRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { useAuth } = require("../../AuthContext"); 
+const { useAuth } = require("../../../EliteCode/AuthContext"); 
 
 
 // Define your routes
