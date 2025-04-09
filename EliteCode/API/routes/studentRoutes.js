d60a6a237201467d5496899a65deb1ee2217d07e
evillalpando5 <146878902+evillalpando5@users.noreply.github.com>
2025-04-09 09:59:20 -0500

    idk

--- EliteCode/API/routes/studentRoutes.js
+++ EliteCode/API/routes/studentRoutes.js
@@ -1,46 +1,27 @@
 const express = require('express');
-const mysql = require('mysql2');
 const router = express.Router();
-const cors = require('cors');
 const db = require('../db');
-const multer = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-app.use(cors());
-app.use(express.json());
-
-
 // Define your routes
 router.get('/', (req, res) => {
   res.send('Student route');
 });
 
-router.get('/questions', (req, res) => {
-  const cid = req.query.cid;
-  const sql = 'SELECT DISTINCT q.qid, q.question, q.description, q.pointVal, q.imgFile, q.topic, q.type, q.dueDate, atc.viewable as classView ' +
-    'From Questions q RIGHT JOIN AssignedToClass atc on q.qid = atc.qid Where cid = ?';
-  db.query(sql, [cid], (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json({ results });
-  });
-  
-});
-router.post('/submitQuestion', (req, res) => {
-  const { qid, sid, answer,progress, submitted_on} = req.body;
-  const imgFile = req.file ? req.file.buffer : null;
-  const sql = `
-    INSERT INTO Submissions (qid, sid, answer,progress, submitted_on, imgFile)
-    VALUES (?, ?, ?, ?, ?, ?)`;
-  db.query(sql, [qid, sid, answer,progress, submitted_on,imgFile], (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json({ results });
-  });
-});
+// router.get('/questions', (req, res) => {
+//   const sid = req.query.sid;
+//   const tid = req.query.tid;
+//   const sql = 'SELECT DISTINCT q.*, ats.viewable as studentView, atc.viewable as classView FROM Questions q ' +
+//   'LEFT JOIN AssignedToStudent ats ON q.qid = ats.qid ' + 
+//   'LEFT JOIN AssignedToClass atc ON q.qid = atc.qid ' +
+//   'LEFT JOIN Enrolled e ON atc.cid = e.cid ' + 
+//   'LEFT JOIN Classes c ON e.cid = c.cid ' +
+//   'LEFT JOIN Instructor i ON c.tid = i.tid WHERE (ats.sid = ? AND i.tid = ?) OR (e.sid = ? AND i.tid = ?);'
+//   db.query(sql, [sid, tid, sid, tid], (err, results) => {
+//     if (err) {
+//       return res.status(500).json({ error: err.message });
+//     }
+//     res.json({ results });
+//   });
+// });
 
 router.post('/joinCourse', async (req, res) => {
   const { sid, cid } = req.body;
