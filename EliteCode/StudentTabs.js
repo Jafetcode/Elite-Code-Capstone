6771cfd3b325130e98740da6f288bc4575ab0232
evanwells34 <ewells0709@gmail.com>
2025-04-09 13:09:05 -0500

    Icons for botton tab

--- EliteCode/StudentTabs.js
+++ EliteCode/StudentTabs.js
@@ -3,18 +3,21 @@ import StudentStack from './StudentStack';
 import StudentProfile from './StudentView/StudentProfile';
 import StudentSettings from './StudentView/StudentSettings';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { Icon } from '@ui-kitten/components';
 const Tab = createBottomTabNavigator();
+const TabIcon = (name) => ({ focused, size }) => (
+  <Icon name={name} fill={focused ? '#3366FF' : '#8F9BB3'} style={{ width: size, height: size }} />
+);
 
 const StudentTabs = () => (
   <Tab.Navigator lazy={true}>
     <Tab.Screen 
       name="Home" 
       component={StudentStack} 
-      options={{ headerShown: false }}
+      options={{ tabBarIcon: TabIcon('home-outline'), headerShown: false }}
     />
-    <Tab.Screen name="Profile" component={StudentProfile} />
-    <Tab.Screen name="Settings" component={StudentSettings} />
+    <Tab.Screen name="Profile" component={StudentProfile} options = {{tabBarIcon: TabIcon('person-outline')}}/>
+    <Tab.Screen name="Settings" component={StudentSettings} options={{ tabBarIcon: TabIcon('settings-outline') }}/>
   </Tab.Navigator>
 );
 
