f51365ab4e5091c19d671f01de43918e311c48c9
evillalpando5 <146878902+evillalpando5@users.noreply.github.com>
2025-04-15 18:00:51 -0500

    Update studentRoutes.js

--- EliteCode/API/routes/studentRoutes.js
+++ EliteCode/API/routes/studentRoutes.js
@@ -232,8 +232,8 @@ router.get("/getPastDueCourseQuestions", async (req, res) => {
 });
 
 router.get('/submission', (req, res) => {
-  const { sid } = req.query.sid;
-  const { qid } = req.query.qid;
+  const sid = req.params.sid;
+  const qid = req.params.qid;
   const sql = 'SELECT * FROM Submissions s join Questions q on s.qid = q.qid WHERE s.qid = ? and s.sid = ?;';
 
   db.query(sql, [qid, sid], (err, results) => {
