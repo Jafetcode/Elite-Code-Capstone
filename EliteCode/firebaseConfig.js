9cb666fbf2675c163d971b6e05e1c28b6e37edfe
Andry20021 <106783235+Andry20021@users.noreply.github.com>
2025-04-15 23:26:12 -0500

    add async storage so users stay logged in after app restart

--- EliteCode/firebaseConfig.js
+++ EliteCode/firebaseConfig.js
@@ -1,14 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, setPersistence, getReactNativePersistence } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCrg8HnqRBqORmaL1GSZqgnxU-_Kmcf3Ac",
   authDomain: "elitecode-733d3.firebaseapp.com",
@@ -19,17 +13,12 @@ const firebaseConfig = {
   measurementId: "G-LKRJY7K7VJ"
 };
 
-// Initialize Firebase
 const FIREBASE_APP = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-const FIREBASE_DB = getFirestore(FIREBASE_APP);
 
-setPersistence(FIREBASE_AUTH, getReactNativePersistence(AsyncStorage))
-  .then(() => {
-    console.log("Firebase auth persistence set to React Native persistence.");
-  })
-  .catch((error) => {
-    console.error("Error setting Firebase auth persistence:", error);
-  });
+const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+const FIREBASE_DB = getFirestore(FIREBASE_APP);
 
 export { FIREBASE_APP, FIREBASE_AUTH, FIREBASE_DB };
